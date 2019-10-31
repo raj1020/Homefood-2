ec3d9f14328d9edf68b7ea3197483b45907a2324
raj1020 <raj.1020@hotmail.com>
2019-10-31 15:46:29 +1100

    added single login for all the users (without proper authorization) and added code to make the webpage stay where the user was before logging in

---
+++ app/controllers/eaters_controller.rb
@@ -0,0 +1,13 @@
+class EatersController < ApplicationController
+
+  
+
+
+     
+
+    
+
+
+end
+
+
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,38 @@
+class PagesController < ApplicationController
+   
+    
+    
+
+    def home 
+
+
+    end 
+    
+    def cook 
+
+    end  
+
+    def order
+
+
+    end 
+    
+    def food 
+
+
+    end
+
+    def delivery 
+
+    end   
+    
+
+    
+     
+
+    
+
+
+end
+
+
